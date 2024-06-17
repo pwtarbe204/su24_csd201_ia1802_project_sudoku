4841269972f30230a390be96dfd5dd9ab7c0bd27
k33ps1lence <tinbqce180935@fpt.edu.vn>
2024-06-17 21:00:09 +0700

    update

--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -16,6 +16,7 @@ public class Sudoku {
      */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!2");
     }
     
 }
